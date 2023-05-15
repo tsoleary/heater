533ab169226d282f6aeedb66ad23e97455dacc7e
tsoleary <tsoleary12@gmail.com>
2023-05-15 15:08:10 -0400

    Book updated with QC figures.

---
+++ src/03_seurat/01_create_seurat_object_filtered.R
@@ -0,0 +1,66 @@
+# ------------------------------------------------------------------------------
+# Load data and set up Seurat object from Cell Ranger ARC filtered matrix
+# May 11, 2023
+# TS O'Leary
+# ------------------------------------------------------------------------------
+
+# Description 
+# Load in the RNA-Seq count data and the ATAC-Seq peak fragment data and save as
+# an rds file to be used for quality control and filtering.
+
+# Load libraries
+library(tidyverse)
+library(Seurat)
+library(Signac)
+library(AnnotationHub)
+
+# Load the RNA-Seq and ATAC-Seq data -------------------------------------------
+data_dir <- here::here("data/processed/seq/all/outs/")
+filtered_feature_dir <- paste0(data_dir, "filtered_feature_bc_matrix")
+frag_path <- paste0(data_dir, "atac_fragments.tsv.gz")
+counts <- Read10X(filtered_feature_dir)
+
+# Get gene annotations for dm6 -------------------------------------------------
+BDGP6.32 <- query(
+  AnnotationHub(), 
+  c("EnsDb", "Drosophila melanogaster", "109")
+)
+annotation <- GetGRangesFromEnsDb(BDGP6.32[[1]])
+seqlevelsStyle(annotation) <- "UCSC"
+
+# Meta data --------------------------------------------------------------------
+# Create meta data for all cells based on the GEM well suffix. From 10X Genomics
+# "This number, which indicates which GEM well the barcode sequence came from, 
+# is called the GEM well suffix. The numbering of the GEM wells will reflect the 
+# order that the GEM wells were provided in the Aggregation CSV."
+meta_data <- tibble(cellnames = colnames(counts[[1]])) %>%
+  mutate(orig.ident = str_extract(cellnames, "[1-4]")) %>%
+  full_join(tibble::tribble(
+    ~orig.ident, ~sample_name, ~acc_temp,
+    "1", "18C_Rep1", "18C",
+    "2", "18C_Rep2", "18C",
+    "3", "25C_Rep1", "25C",
+    "4", "25C_Rep2", "25C"
+  ), 
+  by = "orig.ident") %>%
+  column_to_rownames("cellnames")
+
+# Create a Seurat object containing the RNA data
+dat <- CreateSeuratObject(
+  project = "heater",
+  counts = counts$`Gene Expression`,
+  assay = "RNA",
+  names.delim = "-",
+  meta.data = meta_data
+)
+
+# Create ATAC assay and add it to the object
+dat[["ATAC"]] <- CreateChromatinAssay(
+  counts = counts$Peaks,
+  sep = c(":", "-"),
+  fragments = frag_path,
+  annotation = annotation
+)
+
+# Save raw Seurat object
+saveRDS(dat, here::here("data/processed/seurat_object/01_dat_filtered.rds"))
--- src/03_seurat/02_quality_control_filtering.R
+++ src/03_seurat/02_quality_control_filtering.R
@@ -8,20 +8,53 @@
 # Basic quality control metrics and filtering out non-cell barcodes.
 
 # Load libraries
+library(tidyverse)
 library(Seurat)
 library(Signac)
-library(tidyverse)
 
 # Load  data
 dat <- readRDS(here::here("data/processed/seurat_object/01_dat_raw.rds"))
 
+# Load 10X filtered data
+dat_10x <- readRDS(
+  here::here("data/processed/seurat_object/01_dat_filtered.rds")
+)
 
-# Per sample raw sequencing read data information
+# Total number of barcodes in the data set
+length(Cells(dat))
+
+# Define count cutoffs
+low_ATAC <- 800
+low_RNA <- 350
+
+# Visualize count cutoffs ----
+
+# Density distribution of ATAC Reads
 dat@meta.data %>%
-  group_by(sample_name) %>%
-  summarize(total_RNA_Reads = sum(nCount_RNA),
-            total_ATAC_Reads = sum(nCount_ATAC))
+  filter(nCount_ATAC > 100) %>%
+  ggplot() +
+  geom_density(aes(x = nCount_ATAC)) +
+  geom_vline(aes(xintercept = low_ATAC),
+             color = "red",
+             linetype = 2) +
+  scale_x_continuous(trans = "log10",
+                     breaks = c(1, 10, 100, 1000, 10000),
+                     labels = c("1", "10", "100", "1k", "10k")) +
+  cowplot::theme_minimal_grid()
 
+# Density distribution of RNA Reads
+dat@meta.data %>%
+  filter(nCount_RNA > 100) %>%
+  ggplot() +
+  geom_density(aes(x = nCount_RNA)) +
+  geom_vline(aes(xintercept = low_RNA),
+             color = "red",
+             linetype = 2) +
+  scale_x_continuous(trans = "log10",
+                     breaks = c(1, 10, 100, 1000, 10000),
+                     labels = c("1", "10", "100", "1k", "10k")) +
+  cowplot::theme_minimal_grid()
+  
 # Knee-plot for ATAC -----------------------------------------------------------
 # Create ranked data.frame
 knee_atac_df <- tibble(
@@ -30,26 +63,33 @@ knee_atac_df <- tibble(
   ) %>%
   rownames_to_column("rank") %>%
   mutate(rank = as.numeric(rank)) %>%
-  head(40000)
+  head(100000)
 
 # Plot the knee_plot
-knee_atac_df %>%
+p1 <- knee_atac_df %>%
   ggplot() +
   geom_point(aes(x = rank,
                  y = nCount_ATAC),
              color = "grey50",
              shape = 21) +
-  geom_hline(aes(yintercept = 10000),
+  geom_vline(aes(xintercept = 16000),
              linetype = 2,
              color = "orange",
              alpha = 0.8,
              linewidth = 1.1) +
-  geom_hline(aes(yintercept = 500),
+  geom_hline(aes(yintercept = low_ATAC),
              linetype = 2,
-             color = "orange",
+             color = "red",
              alpha = 0.8,
              linewidth = 1.1) +
-  scale_y_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10", 
+                     breaks = c(100, 1000, 10000, 100000),
+                     labels = c("100", "1k", "10k", "100k"),
+                     name = "ATAC counts") +
+  scale_x_continuous(trans = "log10",
+                     breaks = c(1, 10, 100, 1000, 10000, 100000),
+                     labels = c("1", "10", "100", "1k", "10k", "100k"),
+                     name = "Barcodes rank ordered by total number of ATAC fragments") +
   cowplot::theme_minimal_grid()
 
 # Knee-plot for RNA ------------------------------------------------------------
@@ -60,41 +100,118 @@ knee_rna_df <- tibble(
   ) %>%
   rownames_to_column("rank") %>%
   mutate(rank = as.numeric(rank)) %>%
-  head(40000)
+  head(100000)
 
 # Plot the knee_plot
-knee_rna_df %>%
+p2 <- knee_rna_df %>%
   ggplot() +
   geom_point(aes(x = rank,
                  y = nCount_RNA),
              color = "grey50",
              shape = 21) +
-  geom_hline(aes(yintercept = 10000),
+  geom_vline(aes(xintercept = 16000),
              linetype = 2,
              color = "orange",
              alpha = 0.8,
              linewidth = 1.1) +
-  geom_hline(aes(yintercept = 500),
+  geom_hline(aes(yintercept = low_RNA),
              linetype = 2,
-             color = "orange",
+             color = "red",
              alpha = 0.8,
              linewidth = 1.1) +
-  scale_y_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10", 
+                     breaks = c(100, 1000, 10000, 100000),
+                     labels = c("100", "1k", "10k", "100k"),
+                     name = "RNA counts") +
+  scale_x_continuous(trans = "log10",
+                     breaks = c(1, 10, 100, 1000, 10000, 100000),
+                     labels = c("1", "10", "100", "1k", "10k", "100k"),
+                     name = "Barcodes rank ordered by total number of RNA reads") +
   cowplot::theme_minimal_grid()
 
-# Save knee plot for the 
-ggsave(here::here("output/figs/rna_knee_plot.pdf"))
+# Knee plots together
+cowplot::plot_grid(p1, p2,
+                   nrow = 2)
+
+# Save knee plot
+ggsave(here::here("output/figs/qc/knee_plots.pdf"),
+       height = 6,
+       width = 6)
+
+# Scatter plot of ATAC & RNA counts --------------------------------------------
+
+# Cells called by Cell Ranger ARC
+dat@meta.data %>%
+  filter(nCount_RNA >= 1 &
+           nCount_ATAC >= 1) %>%
+  rownames_to_column("cells") %>%
+  mutate(cells_10x = ifelse(cells %in% Cells(dat_10x), 
+                            "Cell Ranger ARC cells", 
+                            "Non-cell barcodes")) %>%
+  mutate(cells_10x = factor(cells_10x,
+                            levels = c("Non-cell barcodes",
+                                       "Cell Ranger ARC cells"))) %>%
+  ggplot(aes(x = nCount_RNA,
+             y = nCount_ATAC)) +
+  geom_point(aes(color = cells_10x),
+             shape = 21) +
+  scale_x_continuous(trans = "log10", 
+                     breaks = c(1, 10, 100, 1000, 10000, 100000),
+                     labels = c("1", "10", "100", "1k", "10k", "100k"),
+                     name = "RNA counts") +
+  scale_y_continuous(trans = "log10", 
+                     breaks = c(1, 10, 100, 1000, 10000, 100000),
+                     labels = c("1", "10", "100", "1k", "10k", "100k"),
+                     name = "ATAC counts") +
+  scale_color_manual(values = c("grey90", "#91AB73"),
+                     name = element_blank()) +
+  cowplot::theme_minimal_grid() +
+  theme(legend.position = "bottom")
+
+# Save scatter plot
+ggsave(here::here("output/figs/qc/scatter_rna_atac_10x_cells.pdf"),
+       height = 6,
+       width = 6)
+
+# Cells called based on count cutoffs only
+dat@meta.data %>%
+  filter(nCount_RNA >= 1 &
+           nCount_ATAC >= 1) %>%
+  mutate(cells = ifelse(nCount_RNA > low_RNA & 
+                          nCount_ATAC > low_ATAC,
+                        "Cells",
+                        "Non-cell barcodes")) %>%
+  mutate(cells = factor(cells,
+                        levels = c("Non-cell barcodes",
+                                   "Cells"))) %>%
+  ggplot(aes(x = nCount_RNA,
+             y = nCount_ATAC)) +
+  geom_point(aes(color = cells),
+             shape = 21) +
+  scale_x_continuous(trans = "log10", 
+                     breaks = c(1, 10, 100, 1000, 10000, 100000),
+                     labels = c("1", "10", "100", "1k", "10k", "100k"),
+                     name = "RNA counts") +
+  scale_y_continuous(trans = "log10", 
+                     breaks = c(1, 10, 100, 1000, 10000, 100000),
+                     labels = c("1", "10", "100", "1k", "10k", "100k"),
+                     name = "ATAC counts") +
+  scale_color_manual(values = c("grey90", "#91AB73"),
+                     name = element_blank()) +
+  cowplot::theme_minimal_grid() +
+  theme(legend.position = "bottom")
+
+# Save scatter plot 
+ggsave(here::here("output/figs/qc/scatter_rna_atac_low_density_cutoff.pdf"),
+       height = 6,
+       width = 6)
 
 # Filter out low quality cells and non-cell barcodes ---------------------------
 
-# Filter criteria just based on ATAC?
-dat <- subset(
-  x = dat,
-  subset = nCount_ATAC < 10000 &
-    nCount_ATAC > 500 &
-    nCount_RNA > 100 &
-    nCount_RNA < 5000
-)
+# Filter criteria just based on low count threshold
+dat <- dat %>%
+  subset(nCount_ATAC > low_ATAC &
+           nCount_ATAC > low_RNA)
 
 # Total number of nuclei
 dat@meta.data %>%
@@ -110,50 +227,45 @@ dat@meta.data %>%
   group_by(sample_name) %>%
   tally()
 
-# Does calderon have these knee plots -- if not I should just make them if it is possible
-# Albright has good knee plots but that is pre cellularization so it is possible 
-# is why they were able to reduce ambient RNA in their sample that 
-
-
 # DecontX to account for ambient RNAs ------------------------------------------ 
 
-# Convert RNA counts to a single cell experiment object 
-counts <- GetAssayData(object = dat, slot = "counts")
-sce <- SingleCellExperiment::SingleCellExperiment(list(counts = counts))
-
-# Run decontX on the RNA counts
-sce <- celda::decontX(sce)
-
-# Create a new assay object with dexcontX fixed counts
-# Optionally write over the old counts ---??
-dat[["decontX_counts"]] <- CreateAssayObject(
-  counts = celda::decontXcounts(sce)
-)
-
-# Save data
-saveRDS(dat, "data/processed/seurat_object/02_dat_cells.rds")
-
-### SCRATCH
-# Calculate Nucleosome Signal and TSS Enrichment for ATAC QC metrics
-# DefaultAssay(dat) <- "ATAC"
-# dat <- NucleosomeSignal(dat)
-# dat <- TSSEnrichment(dat) # Doesn't work?
-
-# Create simple Violin Plot with cell meta.data information before filtering
-# VlnPlot(
-#   object = dat,
-#   features = c("nCount_RNA", 
-#                "nCount_ATAC", 
-#                "TSS.enrichment", 
-#                "nucleosome_signal"),
-#   ncol = 4,
-#   pt.size = 0
+# # Convert RNA counts to a single cell experiment object 
+# counts <- GetAssayData(object = dat, slot = "counts")
+# sce <- SingleCellExperiment::SingleCellExperiment(list(counts = counts))
+# 
+# # Run decontX on the RNA counts
+# sce <- celda::decontX(sce)
+# 
+# # Create a new assay object with dexcontX fixed counts
+# # Optionally write over the old counts ---??
+# dat[["decontX_counts"]] <- CreateAssayObject(
+#   counts = celda::decontXcounts(sce)
 # )
-
-# VlnPlot(
-#   object = dat,
-#   features = c("nCount_RNA",
-#                "nCount_ATAC"),
-#   ncol = 4,
-#   pt.size = 0
-# )
+# 
+# # Save data
+# saveRDS(dat, "data/processed/seurat_object/02_dat_cells.rds")
+# 
+# ### SCRATCH
+# # Calculate Nucleosome Signal and TSS Enrichment for ATAC QC metrics
+# # DefaultAssay(dat) <- "ATAC"
+# # dat <- NucleosomeSignal(dat)
+# # dat <- TSSEnrichment(dat) # Doesn't work?
+# 
+# # Create simple Violin Plot with cell meta.data information before filtering
+# # VlnPlot(
+# #   object = dat,
+# #   features = c("nCount_RNA", 
+# #                "nCount_ATAC", 
+# #                "TSS.enrichment", 
+# #                "nucleosome_signal"),
+# #   ncol = 4,
+# #   pt.size = 0
+# # )
+# 
+# # VlnPlot(
+# #   object = dat,
+# #   features = c("nCount_RNA",
+# #                "nCount_ATAC"),
+# #   ncol = 4,
+# #   pt.size = 0
+# # )
