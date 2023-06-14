149fd4436266e5132aa74d786b72547d41acff91
tsoleary <tsoleary12@gmail.com>
2023-06-14 14:32:35 -0400

    Update analysis

--- src/03_seurat/11_pseudobulk_DESeq2.R
+++ src/03_seurat/11_pseudobulk_DESeq2.R
@@ -53,30 +53,3 @@ degs <- res |>
 saveRDS(res, here::here("output/degs/pseudobulk_DESeq_res.rds"))
 saveRDS(degs, here::here("output/degs/pseudobulk_DESeq.rds"))
 write_csv(degs |> select(gene), here::here("output/degs/pseudobulk_DESeq_gene_names.csv"))
-
-
-# Quick volcano plot
-p <- res |>
-  as_tibble(rownames = "gene") |>
-  ggplot(aes(label = gene)) +
-  geom_hline(yintercept = -log10(0.05),
-             color = "grey50",
-             linetype = 2) +
-  geom_point(aes(y = -log10(padj),
-                 x = log2FoldChange,
-                 fill = padj < 0.05,
-                 size = padj < 0.05),
-             color = "grey90",
-             stroke = 0.2,
-             shape = 21) +
-  scale_size_manual(values = c(1, 2)) +
-  scale_fill_manual(values = c("grey80", "firebrick")) +
-  scale_y_continuous(expand = c(0, 0.5),
-                     breaks = c(0, 5, 10, 15),
-                     labels = c("0", "5", "10", "15")) +
-  #ggh4x::coord_axes_inside(labels_inside = TRUE) +
-  cowplot::theme_cowplot() +
-  theme(legend.position = "none",
-        axis.title.x = element_blank())
-
-plotly::ggplotly(p, tooltip = c("gene"))
